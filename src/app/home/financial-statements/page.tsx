3f847244ed1f79fec1244c0b034159a0cf55f852
ayazici21 <ayazici21@ku.edu.tr>
2024-06-07 13:39:59 +0300

    Center buttons

--- src/app/home/financial-statements/page.tsx
+++ src/app/home/financial-statements/page.tsx
@@ -2,18 +2,24 @@
 
 import {Button} from "primereact/button";
 import {useRouter} from "next/navigation";
+import {Card} from "primereact/card";
 
 const StatementsPage = () => {
     const router = useRouter();
     return (
-        <div className="w-9 md:w-8 lg:w-6 h-screen flex m-auto">
-            <div className="m-auto">
-                <div className="p-buttonset w-full">
-                    <Button label="Balance Sheet" className="p-button-primary" onClick={() => router.push("/home/financial-statements/balance-sheet")}/>
-                    <Button label="Income Statement" className="p-button-primary mx-3" onClick={() => router.push("/home/financial-statements/income-statement")}/>
-                    <Button label="Statement of Owner's Equity" className="p-button-primary" onClick={() => router.push("/home/financial-statements/owners-equity")}/>
+        <div className="w-12 h-screen flex justify-content-center align-items-center">
+            <Card title="Financial Statements" className="w-full h-full ">
+                <div className="flex justify-content-center align-items-center h-full">
+                    <div className="w-9 md:w-8 lg:w-6 flex flex-column align-items-center gap-3 m-auto">
+                        <Button label="Balance Sheet" className="p-button-primary w-full"
+                                onClick={() => router.push("/home/financial-statements/balance-sheet")}/>
+                        <Button label="Income Statement" className="p-button-primary w-full"
+                                onClick={() => router.push("/home/financial-statements/income-statement")}/>
+                        <Button label="Statement of Owner's Equity" className="p-button-primary w-full"
+                                onClick={() => router.push("/home/financial-statements/owners-equity")}/>
+                    </div>
                 </div>
-            </div>
+            </Card>
         </div>
     )
 }
